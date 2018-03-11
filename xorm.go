c315c1ec7d02e9ebdb61b2ee46a43ca437f6cd8a
ybq <wmdandme@gmail.com>
2018-03-11 23:32:53 +0800

    ok

--- xorm.go
+++ xorm.go
@@ -35,6 +35,7 @@ func regDrvsNDialects() bool {
 		"sqlite3":  {"sqlite3", func() core.Driver { return &sqlite3Driver{} }, func() core.Dialect { return &sqlite3{} }},
 		"oci8":     {"oracle", func() core.Driver { return &oci8Driver{} }, func() core.Dialect { return &oracle{} }},
 		"goracle":  {"oracle", func() core.Driver { return &goracleDriver{} }, func() core.Dialect { return &oracle{} }},
+		"sqlmock":  {"sqlmock", func() core.Driver { return &sqlmockDriver{} }, func() core.Dialect { return &sqlmock{} }},
 	}
 
 	for driverName, v := range providedDrvsNDialects {
